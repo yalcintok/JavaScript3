4a98eea65e4702f265518448e03f1dceb84596c8
Yalcin TOK <yalcintok33@gmail.com>
2019-11-05 12:36:41 +0100

    Some mistakes are fixed

--- Week1/homework/js-exercises/image.js
+++ Week1/homework/js-exercises/image.js
@@ -1,20 +1,24 @@
 'use strict';
 {
-let xhr = new XMLHttpRequest ();
-xhr.open('GET', 'https://picsum.photos/400');
-xhr.onload = function () {
-    if (this.status >= 200 && this.status < 300) {
-        document.getElementById('xhr').setAttribute('src', this.responseURL);
-    } else {
-        console.log('The server sends a : ', this.status);
-    };
-};
 
-xhr.onerror = function () {
-    console.log('There was a connection error!!');
-}
+(function(){
 
-xhr.send();
+        let xhr = new XMLHttpRequest ();
+        xhr.open('GET', 'https://picsum.photos/400');
+        xhr.onload = function () {
+            if (this.status >= 200 && this.status < 300) {
+                document.getElementById('xhr').setAttribute('src', this.responseURL);
+            } else {
+                console.log('The server sends a : ', this.status);
+            };
+        };
+
+        xhr.onerror = function () {
+            console.log('There was a connection error!!');
+        }
+
+        xhr.send();
+})()
 
 
 axios.get('https://picsum.photos/400')
